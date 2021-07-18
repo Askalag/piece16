6301a798fc4162361f1eb75311494a62126137ed
m.bahar <m.bahar@ctt.com.rzd>
2021-07-18 22:09:26 +0300

    upd+

--- src/handler/response.go
+++ src/handler/response.go
@@ -21,14 +21,24 @@ func errorResponse(httpCode int, c *gin.Context, errCode int, errMsg string) {
 	res := NewSimpleResponse()
 	res.ErrorCode = errCode
 	res.ErrorMessage = errMsg
-	c.AbortWithStatusJSON(httpCode, res)
+	c.AbortWithStatusJSON(httpCode, res.toJSON())
 }
 
 func okResponse(c *gin.Context, data []map[string]interface{}) {
 	res := NewSimpleResponse()
 	res.Success = true
 	res.Data = data
-	c.JSON(http.StatusOK, res)
+	c.JSON(http.StatusOK, res.toJSON())
+}
+
+func (r *SimpleResponse) toJSON() map[string]interface{} {
+	return map[string]interface{}{
+		"success": r.Success,
+		"errCode": r.ErrorCode,
+		"errMsg":  r.ErrorMessage,
+		"ver":     r.Ver,
+		"data":    r.Data,
+	}
 }
 
 func NewSimpleResponse() *SimpleResponse {
